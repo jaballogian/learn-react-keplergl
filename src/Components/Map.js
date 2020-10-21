dd4d8b20e30a620405be231502dc67f04e3ffc8f
jaballogian <jaballogianrma77@gmail.com>
2020-10-21 13:51:30 +0700

    fetched data from url in the Map component

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import KeplerGl from "kepler.gl"
 
 function Map(){
 
     const REACT_APP_MAPBOX_API = "pk.eyJ1IjoiamFiYWxsb2dpYW4iLCJhIjoiY2tnaXp5NDF3MGJ1NjJ0cGVuczh2b3hpcyJ9.DCwkQRt9dEGAy5lfiLAugw"
-    
+
+    const url = "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
+    useEffect(() => {
+        fetch(url)
+            .then((response) => {
+                return(
+                    response.json()
+                )
+            })
+            .then(responseJson => {
+                console.log(responseJson)
+            })
+    }, [])
+
     return(
         <KeplerGl
             id="covid"
