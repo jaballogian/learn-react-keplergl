b7a7e60ea38e2b0aba595b70dbdd50848170cddc
jaballogian <jaballogianrma77@gmail.com>
2020-10-21 13:37:19 +0700

    added Kepler gl map component to the Map component

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import KeplerGl from "kepler.gl"
 
 function Map(){
+
+    const REACT_APP_MAPBOX_API = "pk.eyJ1IjoiamFiYWxsb2dpYW4iLCJhIjoiY2tnaXp5NDF3MGJ1NjJ0cGVuczh2b3hpcyJ9.DCwkQRt9dEGAy5lfiLAugw"
+    
     return(
-        <div>
-            Map
-        </div>
+        <KeplerGl
+            id="covid"
+            mapboxApiAccessToken={REACT_APP_MAPBOX_API}
+            width={window.innerWidth}
+            heigth={window.innerHeight}
+        />
     )
 }
 
