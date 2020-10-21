576c6d009fdf97b4d4382277e9601344dc021acf
jaballogian <jaballogianrma77@gmail.com>
2020-10-21 12:48:26 +0700

    added styled components dependency to the project and imported some libraries to the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import keplerGlReducer from "kepler.gl/reducers"
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import { taskMiddleware } from "react-palm/tasks"
+import { Provider, useDispatch } from "react-redux"
+import KeplerGl from "kepler.gl"
+import { addDataToMap } from "kepler.gl/actions"
+import useSwr from "swr"
 
 function App() {
   return (
     <div>
-      
+      App
     </div>
   );
 }
 
-export default App;
+export default App
